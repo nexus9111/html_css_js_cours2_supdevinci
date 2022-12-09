c34aaab5d4f6eaaee5bb78089a246ee388aed766
J.Nexus <46869823+nexus9111@users.noreply.github.com>
2022-12-09 10:53:22 +0100

    :tada: add classes

--- cours2_cocktailApp/app/controllers/main.js
+++ cours2_cocktailApp/app/controllers/main.js
@@ -81,3 +81,112 @@ const createRandomCocktailCard = async () => {
 
 button.addEventListener('click', createRandomCocktailCard);
 
+/* -------------------------------------------------------------------------- */
+/*                                NEW EXERCISE                                */
+/* -------------------------------------------------------------------------- */
+
+const workerButton = createElement('button', ['btn', 'btn-success'], 'Display all cocktails!');
+
+workerButton.addEventListener('click', () => {
+    const worker = new Worker();
+    worker.createCocktailsCard();
+});
+
+class Cocktail {
+    constructor(drink) {
+        this.image = drink.strDrinkThumb;
+        this.name = drink.strDrink;
+        this.subtitle = drink.strCategory;
+        this.description = drink.strInstructions;
+        this.ingredients = this.getIngredients(drink);
+    }
+
+    getCocktailsInfo() {
+        return {
+            image: this.image,
+            name: this.name,
+            subtitle: this.subtitle,
+            description: this.description,
+            ingredients: this.ingredients
+        } 
+    }
+
+    getIngredients(drink) {
+        let ingredients = [];
+        for (const key in drink) {
+            if (key.includes('strIngredient') && drink[key]) {
+                ingredients.push(drink[key]);
+            }
+        }
+        // make all ingredients unique
+        ingredients = [...new Set(ingredients)];
+        return ingredients;
+    }
+}
+
+class Worker {
+    createCocktailsCard = async () => {
+        try {
+            const cocktailInfo = await this.getCocktailsInfo();
+            let cocktails = [];
+            for (const drink of cocktailInfo) {
+                const cocktail = new Cocktail(drink);
+                cocktails.push(cocktail);
+            }
+            const cardContent = this.createCocktailCardContent(cocktails);
+            root.innerHTML = cardContent;
+        } catch (error) {
+            alert(error);
+        }
+    }
+
+    createCocktailCardContent = (cocktails) => {
+
+        let result = `
+        <div class="row">
+        `
+
+        for (const cocktail of cocktails) {
+            const cocktailInfo = cocktail.getCocktailsInfo();
+            result += `
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="${cocktailInfo.image}" class="card-img-top" alt="${cocktailInfo.name}">
+                    <div class="card-body">
+                        <h5 class="card-title">${cocktailInfo.name}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${cocktailInfo.subtitle}</h6>
+                        <p>${cocktailInfo.description}</p>
+                        ${this.createIngredientsList(cocktailInfo.ingredients)}
+                    </div>
+                </div>
+            </div>
+            `
+        }
+
+        result += `
+        </div>
+        `
+        return result;
+    }
+
+    createIngredientsList = (ingredients) => {
+        let ingredientsList = `<ol class="list-group list-group-numbered">`
+        for (const ingredient of ingredients) {
+            ingredientsList += `<li class="list-group-item">${ingredient}</li>`
+        }
+        ingredientsList += `</ol>`
+        return ingredientsList;
+    }
+
+    getCocktailsInfo = async () => {
+        try {
+            const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+            const cocktails = await response.json();
+            return cocktails.drinks;
+        } catch (error) {
+            console.log(error);
+            throw new Error("An error occured while fetching the cocktail data");
+        }
+    }
+}
+
